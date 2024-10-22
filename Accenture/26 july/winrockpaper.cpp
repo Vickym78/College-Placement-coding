755c418244a224b9f4306dfcaae9e7dec25abf77
Vicky Mahato <vickymahato2003@gmail.com>
2024-10-22 18:59:57 +0530

    26 JULY ACCENTURE QUESTION

---
+++ Accenture/26 july/winrockpaper.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str ; 
+    cin>>str;
+
+    if(str=="Rock"){
+        cout<<"Paper";
+    }
+    else if(str=="Paper") cout<<"Scissors";
+    else cout<<"rock";
+}
