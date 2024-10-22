a4edaf91a791d7c7d3dcfa869dd01e9048fd763b
Vicky Mahato <vickymahato2003@gmail.com>
2024-10-22 21:03:05 +0530

    12 august ACCENTURE QUESTION

--- Accenture/12 aug/sumprime.cpp
+++ Accenture/12 aug/sumprime.cpp
@@ -10,12 +10,11 @@ bool isprime(int n ){
 
 int main(){
     int n; cin>>n;
-    if(n>2) {cout<<0; return 0;}
+    if(n<2) {cout<<0; return 0;}
     int sum =0; 
     for(int i=2;i<=n;i++){
         if(isprime(i)) sum+=i;
     }
-
     cout<<sum;
 
 }
